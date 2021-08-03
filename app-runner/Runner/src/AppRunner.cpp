f35064d20b4adcc0e2e9402073121c7c9084e884
Paul Warnes <pwarnes@gmail.com>
2021-08-03 13:36:42 -0500

    initial project setup

---
+++ app-runner/Runner/src/AppRunner.cpp
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include "AppRunnerConfig.h"
+
+
+int main (int argc, char *argv[])
+{
+  if (argc < 2)
+  {
+	fprintf(stdout,"%s Version %d.%d\n",
+            argv[0],
+            AppRunner_VERSION_MAJOR,
+            AppRunner_VERSION_MINOR);
+    fprintf(stdout,"Usage: %s number\n",argv[0]);
+    return 1;
+  }
+
+  double inputValue = atof(argv[1]);
+  double outputValue = 
+	 sqrt(inputValue);  
+  
+  fprintf(stdout,"The square root of %g is %g\n",
+          inputValue, outputValue);
+  return 0;
+}
